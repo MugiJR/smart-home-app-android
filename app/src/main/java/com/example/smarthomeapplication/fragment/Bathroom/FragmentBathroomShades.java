a41239a97ba053f41c4f30002fcd7ab0af4cb4f4
MugiJR <immugi@yahoo.com>
2022-11-12 12:32:24 +0100

    Added Backend logic for Shades

--- app/src/main/java/com/example/smarthomeapplication/fragment/Bathroom/FragmentBathroomShades.java
+++ app/src/main/java/com/example/smarthomeapplication/fragment/Bathroom/FragmentBathroomShades.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.smarthomeapplication.R;
+import com.example.smarthomeapplication.fragment.bedroom.FragmentBedroom;
 
 public class FragmentBathroomShades extends Fragment implements View.OnClickListener {
+
+    private SeekBar mSeekbar;
+    private ImageView mBackArrow;
+    private TextView mShadesText;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,7 +35,27 @@ public class FragmentBathroomShades extends Fragment implements View.OnClickList
     }
 
     private void initView(View view) {
+        mSeekbar = view.findViewById(R.id.seekBar_shades);
+        mShadesText = view.findViewById(R.id.textView_shades);
+        mBackArrow = view.findViewById(R.id.back_arrow_shades);
+        mBackArrow.setOnClickListener(this);
 
+        mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mShadesText.setText("" + i + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getActivity(),"Shades has been set to "+ seekBar.getProgress()+ "%",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -33,10 +63,19 @@ public class FragmentBathroomShades extends Fragment implements View.OnClickList
     public void onClick(View view) {
         switch (view.getId())
         {
-
+            case R.id.back_arrow_shades:
+                showTopLevelFragment(new FragmentBathroom());
+                break;
 
 
         }
+    }
+
+    public void showTopLevelFragment(Fragment fragment) {
+        // Use the fragment manager to dynamically change the fragment displayed in the FrameLayout.
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_host_main, fragment)
+                .commit();
     }
 
 }
--- app/src/main/java/com/example/smarthomeapplication/fragment/bedroom/FragmentBedroomShades.java
+++ app/src/main/java/com/example/smarthomeapplication/fragment/bedroom/FragmentBedroomShades.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.smarthomeapplication.R;
+import com.example.smarthomeapplication.fragment.Bathroom.FragmentBathroom;
 
 public class FragmentBedroomShades extends Fragment implements View.OnClickListener {
+
+    private SeekBar mSeekbar;
+    private ImageView mBackArrow;
+    private TextView mShadesText;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,7 +34,27 @@ public class FragmentBedroomShades extends Fragment implements View.OnClickListe
     }
 
     private void initView(View view) {
+        mSeekbar = view.findViewById(R.id.seekBar_shades);
+        mShadesText = view.findViewById(R.id.textView_shades);
+        mBackArrow = view.findViewById(R.id.back_arrow_shades);
+        mBackArrow.setOnClickListener(this);
 
+        mSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mShadesText.setText("" + i + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getActivity(),"Shades has been set to "+ seekBar.getProgress()+ "%",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -34,9 +63,18 @@ public class FragmentBedroomShades extends Fragment implements View.OnClickListe
         switch (view.getId())
         {
 
-
+            case R.id.back_arrow_shades:
+                showTopLevelFragment(new FragmentBathroom());
+                break;
 
         }
+    }
+
+    public void showTopLevelFragment(Fragment fragment) {
+        // Use the fragment manager to dynamically change the fragment displayed in the FrameLayout.
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_host_main, fragment)
+                .commit();
     }
 
 }
