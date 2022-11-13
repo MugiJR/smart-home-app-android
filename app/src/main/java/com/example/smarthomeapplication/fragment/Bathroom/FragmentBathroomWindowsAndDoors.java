d5590b4ff82eca758f77ec43c337bef0761352de
MugiJR <immugi@yahoo.com>
2022-11-13 18:50:35 +0100

    Added Backend logic for WindowsDoors

--- app/src/main/java/com/example/smarthomeapplication/fragment/Bathroom/FragmentBathroomWindowsAndDoors.java
+++ app/src/main/java/com/example/smarthomeapplication/fragment/Bathroom/FragmentBathroomWindowsAndDoors.java
@@ -5,28 +5,60 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.smarthomeapplication.R;
 
 public class FragmentBathroomWindowsAndDoors extends Fragment implements View.OnClickListener {
 
+    private Switch mSwitchWindow, mSwitchDoor;
+    private ImageView mButtonBack;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
 
         View view = inflater.inflate(R.layout.activity_door_and_window, container, false);
-
+        initView(view);
         return view;
 
 
     }
 
     private void initView(View view) {
+        mButtonBack = view.findViewById(R.id.back_arrow_door_window);
+        mSwitchWindow = view.findViewById(R.id.switchWindow);
+        mSwitchDoor = view.findViewById(R.id.switchDoor);
 
+        mButtonBack.setOnClickListener(this);
+
+
+        mSwitchWindow.setOnClickListener(view12 -> {
+            if (mSwitchWindow.isChecked()) {
+                Toast.makeText(getActivity(),"Windows are opening...",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getActivity(),"Windows are closing...",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        mSwitchDoor.setOnClickListener(view1 -> {
+            if (mSwitchDoor.isChecked()) {
+                Toast.makeText(getActivity(),"Doors are opening...",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getActivity(),"Doors are closing...",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -34,10 +66,19 @@ public class FragmentBathroomWindowsAndDoors extends Fragment implements View.On
     public void onClick(View view) {
         switch (view.getId())
         {
-
+            case R.id.back_arrow_door_window:
+                showTopLevelFragment(new FragmentBathroom());
+                break;
 
 
         }
+    }
+
+    public void showTopLevelFragment(Fragment fragment) {
+        // Use the fragment manager to dynamically change the fragment displayed in the FrameLayout.
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_host_main, fragment)
+                .commit();
     }
 
 }
--- app/src/main/java/com/example/smarthomeapplication/fragment/bedroom/FragmentBedroomWindowsAndDoors.java
+++ app/src/main/java/com/example/smarthomeapplication/fragment/bedroom/FragmentBedroomWindowsAndDoors.java
@@ -5,14 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.smarthomeapplication.R;
+import com.example.smarthomeapplication.fragment.Bathroom.FragmentBathroom;
 
 public class FragmentBedroomWindowsAndDoors extends Fragment implements View.OnClickListener {
+
+    private Switch mSwitchWindow, mSwitchDoor;
+    private ImageView mButtonBack;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,7 +33,31 @@ public class FragmentBedroomWindowsAndDoors extends Fragment implements View.OnC
     }
 
     private void initView(View view) {
+        mButtonBack = view.findViewById(R.id.back_arrow_door_window);
+        mSwitchWindow = view.findViewById(R.id.switchWindow);
+        mSwitchDoor = view.findViewById(R.id.switchDoor);
 
+        mButtonBack.setOnClickListener(this);
+
+
+        mSwitchWindow.setOnClickListener(view12 -> {
+            if (mSwitchWindow.isChecked()) {
+                Toast.makeText(getActivity(),"Windows are opening...",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getActivity(),"Windows are closing...",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        mSwitchDoor.setOnClickListener(view1 -> {
+            if (mSwitchDoor.isChecked()) {
+                Toast.makeText(getActivity(),"Doors are opening...",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getActivity(),"Doors are closing...",Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -34,10 +65,19 @@ public class FragmentBedroomWindowsAndDoors extends Fragment implements View.OnC
     public void onClick(View view) {
         switch (view.getId())
         {
-
+            case R.id.back_arrow_door_window:
+                showTopLevelFragment(new FragmentBedroom());
+                break;
 
 
         }
+    }
+
+    public void showTopLevelFragment(Fragment fragment) {
+        // Use the fragment manager to dynamically change the fragment displayed in the FrameLayout.
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_host_main, fragment)
+                .commit();
     }
 
 }
